b6c91078b1874fb8663f8a52ea4e5851aa3a0c83
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-23 11:24:06 +0900

    first

--- README.md
+++
@@ -1,1 +0,0 @@
-# runtecker_mock
--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <Home />
-        <Footer />
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
