9dd3aa7ac6bd032701838e14c3f98891d29d8d2a
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-28 07:08:07 +0900

    scope

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "read:org"
   OmniAuth.config.allowed_request_methods = [:post, :get]
   OmniAuth.config.logger = Rails.logger
 end
